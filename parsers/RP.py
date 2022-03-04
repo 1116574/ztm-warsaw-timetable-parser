a77799ce6949d6b98a740f4d3b085312cfb36a45
Paweł <pawel1116574@gmail.com>
2022-03-04 02:02:56 +0100

    symbols fix

--- parsers/RP.py
+++ parsers/RP.py
@@ -4,10 +4,11 @@ from .TD import TD
 def RP(f):
     regex = r" {15}(\d{6})"  # I could parse the whole line, but why? ITs data is already in ZP
     output = {}
+    symbols_output = {}
     for line in f:
         if line.find('#RP') > -1:
             print(f'    RP-RETURNING')
-            return output, symbols
+            return output, symbols_output
 
         match = re.search(regex, line)
         if match:
@@ -17,6 +18,8 @@ def RP(f):
                 "id": match[1],
                 "deparures": result
             }
+
+            symbols_output[match[1]] = symbols
 
             # now to *TD
     pass
